08b7cd9fc43075bd463fb41193eee8de529c9362
Taylor-Bracone <71147213+Taylor-Bracone@users.noreply.github.com>
2021-09-25 18:34:00 -0400

    styles and bullet pts accomplished

--- hktb/animated-button/src/Animated_button.js
+++ hktb/animated-button/src/Animated_button.js
@@ -10,8 +10,8 @@ export class Animated_button extends LitElement {
         display: inline-block;
         padding: 25px;
         color: var(--animated-button-text-color, #000);
-        --animatedbuttonBackgroundColor: #000;
-        --animatedbuttonTextColor: #ffffff;
+        --animatedbuttonBackgroundColor: #ff7e1d;
+        --animatedbuttonTextColor: #413d45;
         --animatedbuttonFont: sans-serif;
         --disabledBackground: rgba(0, 0, 0, 0.2); /*light grey*/
         --buttonActive: #CBC3E3; /*light purple*/
@@ -23,17 +23,21 @@ export class Animated_button extends LitElement {
         color: var(--animatedbuttonTextColor);
         background-color: var(--animatedbuttonBackgroundColor);
         padding: 0.5rem 2rem;
+        margin-bottom: 10px;
         border: 2px solid var(--animatedbuttonBackgroundColor);
         border-radius: 5px;
         whitespace: nowrap;
         font-family: var(--animatedbuttonFont);
+        font-size: 0.8em;
         text-decoration: none;
         transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
+        box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+        min-width: 200px;
       }
 
       button:hover {
-        color: var(--animatedbuttonBackgroundColor);
-        background-color: transparent;
+        color: rgba(65,61,69,0.6);
+        background-color: #ff9c03;
         text-decoration: none;
         cursor: pointer;
       }
@@ -46,8 +50,9 @@ export class Animated_button extends LitElement {
       }
 
       button:disabled {
-        color: var(--animatedbuttonTextColor);
-        background-color: var(--disabledBackground);
+        /*color: var(--animatedbuttonTextColor);
+        background-color: var(--disabledBackground);*/
+        opacity: 0.6;
         cursor: not-allowed;
       }
 
@@ -71,6 +76,13 @@ export class Animated_button extends LitElement {
         color: var(--animatedbuttonBackgroundColor);
         background-color: transparent;
         text-decoration: none;
+      }
+
+      label {
+        margin-left: 15px;
+        color: var(--animatedbuttonTextColor);
+        font-family: var(--animatedbuttonFont);
+        font-size: 0.8em;
       }
       `;
     }
@@ -96,7 +108,7 @@ export class Animated_button extends LitElement {
     this.link = 'https://www.headspace.com/';
     this.buttonState = false;
     this.accentColor = 'salmon';
-    this.icon = 'hardware:headset';
+    this.icon = 'image:wb-sunny';
     if (this.querySelector('a')) {
       this.link = this.querySelector('a').getAttribute('href');
       this.title = this.querySelector('button').value;
